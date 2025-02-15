48e6add085819d8b70b25900482c3e168883f4c5
Matt <mbarrett@planittesting.com>
2025-02-16 12:28:39 +1300

    tidy contact tests

--- tests/contact-tests.spec.ts
+++ tests/contact-tests.spec.ts
@@ -6,12 +6,6 @@ import { DataProperties } from '../properties/dataProperties.ts';
 test.describe('Contact Form Tests', () => {
     let homePage: HomePage;
 
-    /*async function repeatTest(times: number, testFn: (i: number) => Promise<void>) {
-        for (let i = 1; i <= times; i++) {
-            await testFn(i);
-        }
-    }*/
-
     test.beforeEach(async ({ page }) => {
         homePage = new HomePage(page);
         await homePage.navigateToHomePage();
@@ -26,17 +20,13 @@ test.describe('Contact Form Tests', () => {
         await contactPage.enterForename('Forename');
         await contactPage.enterEmail('email@email.com');
         await contactPage.enterMessage('Test Message');
-        //(await contactPage.getForenameError()).toBe('');
-        //(await contactPage.getEmailError()).toBe('');
-        //(await contactPage.getMessageError()).toBe('');
         expect(await contactPage.isForenameErrorAbsent()).toBe(true);
         expect(await contactPage.isEmailErrorAbsent()).toBe(true);
         expect(await contactPage.isMessageErrorAbsent()).toBe(true);
     });
 
 
-    // for (let i = 0; i < 5; i++) {
-    test('Submit form successfully'/* + i*/, async ({ page, context }) => {
+    test('Submit form successfully', async ({ page, context }) => {
         const contactPage = await homePage.goToContact();
         await contactPage.enterForename('Forename');
         await contactPage.enterEmail('email@email.com');
@@ -44,13 +34,6 @@ test.describe('Contact Form Tests', () => {
         await contactPage.submitForm();
         const successMessage = await contactPage.getSuccessMessage();
         expect(successMessage).toContain('Thanks Forename, we appreciate your feedback.');
-        /*try {
-            //await page.waitForTimeout(50000)
-            // const backButton = page.getByRole('button', { name: 'Back' });
-            await (await contactPage.goBackButton()).click();
-        } catch (error) {
-            console.log(error);
-        }*/
     });
-    //}
+
 });
