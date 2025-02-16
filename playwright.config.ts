3c866075b2bc08489db7233e9a1100e032cee668
Matt <mbarrett@planittesting.com>
2025-02-16 20:53:36 +1300

    try other merge command

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,10 +23,12 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter:  [
-/*['list'], 
+    ['list'], 
     ['junit', { outputFile: 'playwright-report/results.xml' }],
-    ['html', { outputFile: 'playwright-report/results.html' }]*/
-   ['blob', { outputDir: 'playwright-report' }],
+    ['html', { outputFile: 'playwright-report/results.html' }],
+    ['merge', { outputFile: 'playwright-report/merged-results.html' }], 
+    ['merge', { outputFile: 'playwright-report/merged-results.xml' }] 
+    
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
